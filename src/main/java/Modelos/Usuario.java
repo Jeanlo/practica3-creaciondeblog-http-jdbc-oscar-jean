2ad580b100aed9afe25cd959faecb3b531625763
Jeanlo <jeanlotejeda0101@hotmail.com>
2018-06-03 19:59:16 -0400

    feature/t4-Agregando clase usuario.

---
+++ src/main/java/Modelos/Usuario.java
@@ -0,0 +1,57 @@
+/*********************************************************
+ *  Práctica #3 - HTTP - JDBC (Creación de un blog)       *
+ *  Realizada por:                                        *
+ *      - Oscar Dionisio Núñez Siri - 2014-0056           *
+ *      - Jean Louis Tejeda - 2013-1459                   *
+ *  Materia: Programación Web - ISC-415-T-001
+ *********************************************************/
+
+package Modelos;
+
+public class Usuario {
+    long id;
+    String username;
+    String password;
+    boolean adminstrator;
+    boolean autor;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdminstrator() {
+        return adminstrator;
+    }
+
+    public void setAdminstrator(boolean adminstrator) {
+        this.adminstrator = adminstrator;
+    }
+
+    public boolean isAutor() {
+        return autor;
+    }
+
+    public void setAutor(boolean autor) {
+        this.autor = autor;
+    }
+}
