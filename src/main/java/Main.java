bbf74bdd4d2aa828483be835f53a03251b7e8641
OscarDNS <oscardns96@gmail.com>
2018-06-03 19:16:39 -0400

    PProyecto creado y dependencias de Gradle agregadas.

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/*********************************************************
+ *  Práctica #3 - HTTP - JDBC (Creación de un blog)       *
+ *  Realizada por:                                        *
+ *      - Oscar Dionisio Núñez Siri - 2014-0056           *
+ *      - Jean Louis Tejeda - 2013-1459                   *
+ *  Materia: Programación Web - ISC-415-T-001             *
+ *********************************************************/
+
+public class Main {
+}
