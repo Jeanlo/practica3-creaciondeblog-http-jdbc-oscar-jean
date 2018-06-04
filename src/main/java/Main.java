cce7783ad9ec4012f34aa9f7bd57c2dec9ec3cf1
Jeanlo <jeanlotejeda0101@hotmail.com>
2018-06-03 20:42:08 -0400

    Agregando detener el servicio de base de datos.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,3 @@
-import Servicios.ServicioBaseDatos;
-import Servicios.ServicioBootstrap;
-
-import java.sql.SQLException;
-
 /*********************************************************
  *  Práctica #3 - HTTP - JDBC (Creación de un blog)       *
  *  Realizada por:                                        *
@@ -11,17 +6,25 @@ import java.sql.SQLException;
  *  Materia: Programación Web - ISC-415-T-001             *
  *********************************************************/
 
+import Servicios.ServicioBaseDatos;
+import Servicios.ServicioBootstrap;
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
         try {
-            //Iniciando el servicio
+            //Iniciando el servicio de Base de datos
             ServicioBootstrap.startDb();
 
-            //Prueba de Conexión.
+            //Prueba de conexión
             ServicioBaseDatos.getInstancia().testConexion();
 
             //Creando tablas de la Base de datos
             ServicioBootstrap.crearTablas();
+
+            //Deteniendo el servicio de Base de datos
+            ServicioBootstrap.stopDb();
         } catch (SQLException e) {
             e.printStackTrace();
         }
